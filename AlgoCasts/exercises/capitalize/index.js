18a17e525b4983527f7d4bd6225fa1b420ae4a3a
ahza64 <justinmichaelpettit@gmail.com>
2022-02-02 20:58:15 -0800

    capitalize with previous position method. if it's a space in the previous position, caplitalize the current position

--- AlgoCasts/exercises/capitalize/index.js
+++ AlgoCasts/exercises/capitalize/index.js
@@ -7,15 +7,32 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
+// look at previous position method
 function capitalize(str) {
-  let words = []
-  // pull word(s) out of str.split array
-  for (let word of str.split(' ')) {
-    // capitalize the first letter and push into new wrods array
-    words.push(word[0].toUpperCase() + word.slice(1))
+  // automatically capitalize the first position of the whole sentence string
+  let result = str[0].toUpperCase()
+
+  for (var i = 1; i < str.length; i++) {
+    // look at previous position, and if it's a space, then capitalize
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase()
+    } else {
+      result += str[i]
+    }
   }
-  // return the array after joining it back together in a string with spaces
-  return words.join(' ')
+  return result
 }
+
+// split method
+// function capitalize(str) {
+//   let words = []
+//   // pull word(s) out of str.split array
+//   for (let word of str.split(' ')) {
+//     // capitalize the first letter and push into new wrods array
+//     words.push(word[0].toUpperCase() + word.slice(1))
+//   }
+//   // return the array after joining it back together in a string with spaces
+//   return words.join(' ')
+// }
 
 module.exports = capitalize;
