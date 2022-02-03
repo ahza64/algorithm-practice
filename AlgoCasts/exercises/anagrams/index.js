fc00b02e6a360326d33104133a9e8ec415919b46
ahza64 <justinmichaelpettit@gmail.com>
2022-02-02 20:38:11 -0800

    anagrams with sort method and bad time complexity

--- AlgoCasts/exercises/anagrams/index.js
+++ AlgoCasts/exercises/anagrams/index.js
@@ -8,31 +8,40 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-// comparing string count using the Map method
+// simpler with sort method, but bad time complexity
 function anagrams(stringA, stringB) {
-  const charMapA = buildCharMap(stringA)
-  const charMapB = buildCharMap(stringB)
-  // compare the lengths of the maps in case MapB is longer
-  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
-    return false
-  }
-
-  for (let char in charMapA) {
-    // check that each key has the same value
-    if (charMapA[char] !== charMapB[char]) {
-      return false
-    }
-  }
-  return true
+  return cleanString(stringA) === cleanString(stringB)
+}
+// use sort method to order the stings with bad time complexity, then simply compare the two strings.
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
 }
 
-function buildCharMap(str) {
-  const charMap = {}
-  // use regex to remove punctuation and spaces
-  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-    charMap[char] = charMap[char] + 1 || 1
-  }
-  return charMap
-}
+// // comparing string count using the Map method
+// function anagrams(stringA, stringB) {
+//   const charMapA = buildCharMap(stringA)
+//   const charMapB = buildCharMap(stringB)
+//   // compare the lengths of the maps in case MapB is longer
+//   if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+//     return false
+//   }
+//
+//   for (let char in charMapA) {
+//     // check that each key has the same value
+//     if (charMapA[char] !== charMapB[char]) {
+//       return false
+//     }
+//   }
+//   return true
+// }
+//
+// function buildCharMap(str) {
+//   const charMap = {}
+//   // use regex to remove punctuation and spaces
+//   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1
+//   }
+//   return charMap
+// }
 
 module.exports = anagrams;
