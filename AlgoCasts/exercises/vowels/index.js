c86794d203d0ecd81f39bd28de5fda5be7317e62
ahza64 <justinmichaelpettit@gmail.com>
2022-02-03 17:06:27 -0800

    vowels, iterative solution with includes()

--- AlgoCasts/exercises/vowels/index.js
+++ AlgoCasts/exercises/vowels/index.js
@@ -7,6 +7,16 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// iterative with includes()
+function vowels(str) {
+  let counter = 0
+  const checker = ['a', 'e', 'i', 'o', 'u']
+  for (let char of str.toLowerCase()) {
+    if (checker.includes(char)) {
+      counter++
+    }
+  }
+  return counter
+}
 
 module.exports = vowels;
