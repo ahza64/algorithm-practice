733bced2d7a84efd6bf126c684f10b1d4688dd7a
ahza64 <justinmichaelpettit@gmail.com>
2022-02-03 20:01:35 -0800

    fibonacci memoization solution using apply() and cache

--- AlgoCasts/exercises/fib/index.js
+++ AlgoCasts/exercises/fib/index.js
@@ -8,14 +8,39 @@
 // Example:
 //   fib(4) === 3
 
-// recursive solution, bad time complexity O^2
-function fib(n) {
+// memoization solution, using apply() and cache
+function memoize(fn) {
+  const cache = {}
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args]
+    }
+
+    const result = fn.apply(this, args)
+    cache[args] = result
+
+    return result
+  }
+}
+
+function fibSlow(n) {
   if (n < 2) {
     return n
   }
 
   return fib(n - 1) + fib(n -2)
 }
+
+const fib = memoize(fibSlow)
+
+// // recursive solution, bad time complexity O^2
+// function fib(n) {
+//   if (n < 2) {
+//     return n
+//   }
+//
+//   return fib(n - 1) + fib(n -2)
+// }
 
 // // iterative solution
 // function fib(n) {
