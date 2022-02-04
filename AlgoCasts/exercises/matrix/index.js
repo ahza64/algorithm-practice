93dba7f09c32d720bcf8d001848ef73276ad2e0d
ahza64 <justinmichaelpettit@gmail.com>
2022-02-03 19:44:38 -0800

    matrix spiral

--- AlgoCasts/exercises/matrix/index.js
+++ AlgoCasts/exercises/matrix/index.js
@@ -15,6 +15,49 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const results = []
+
+  for (var i = 0; i < n; i++) {
+    results.push([])
+  }
+
+  let counter = 1
+  let columnStart = 0
+  let columnEnd = n - 1
+  let rowStart = 0
+  let rowEnd = n - 1
+
+  while (columnStart <= columnEnd && rowStart <= rowEnd) {
+    // top row
+    for (let i = columnStart; i <= columnEnd; i++) {
+      results[rowStart][i] = counter
+      counter++
+    }
+    rowStart++
+
+    // right column
+    for (let i = rowStart; i <= rowEnd; i++) {
+      results[i][columnEnd] = counter
+      counter++
+    }
+    columnEnd--
+
+    // bottom row
+    for (let i = columnEnd; i >= columnStart; i--) {
+      results[rowEnd][i] = counter
+      counter++
+    }
+    rowEnd--
+
+    // left column
+    for (let i = rowEnd; i >= rowStart; i--) {
+      results[i][columnStart] = counter
+      counter++
+    }
+    columnStart++
+  }
+  return results
+}
 
 module.exports = matrix;
