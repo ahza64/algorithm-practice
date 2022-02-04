79ba37787c7df8c5f9ffebc968bdcbc03f6a97ae
ahza64 <justinmichaelpettit@gmail.com>
2022-02-03 16:35:24 -0800

    pyramid with nested loops, iterative solution

--- AlgoCasts/exercises/pyramid/index.js
+++ AlgoCasts/exercises/pyramid/index.js
@@ -14,6 +14,23 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+
+// double nested loops iterative solution
+function pyramid(n) {
+  // find midpoint to know where the # goes
+  const mid = Math.floor((2 * n - 1)/2)
+  for (let row = 0; row < n; row++) {
+    let str = ''
+    for (let column = 0; column < 2 * n - 1; column++) {
+      // put a hash at midpoint for the first row, and +1 -1non either side of the midpoint for each additional row
+      if (mid - row <= column && mid + row >= column) {
+        str += '#'
+      }else{
+        str += ' '
+      }
+    }
+    console.log(str);
+  }
+}
 
 module.exports = pyramid;
