db95ec2dbf71183604fc1ddf7cb96bb7ad9d4f83
ahza64 <justinmichaelpettit@gmail.com>
2022-02-15 21:27:30 -0800

    binary search trees contains

--- AlgoCasts/exercises/bst/index.js
+++ AlgoCasts/exercises/bst/index.js
@@ -26,13 +26,22 @@ class Node {
     }
     if (data > this.data && this.right) {
       this.right.insert(data)
-    } else if (data > this.data) {  
+    } else if (data > this.data) {
       this.right = new Node(data)
     }
   }
 
   contains(data) {
+    if (this.data === data) {
+      return this
+    }
 
+    if (this.data < data && this.right) {
+      return this.right.contains(data)
+    } else if (this.data > data && this.left) {
+      return this.left.contains(data)
+    }
+    return null
   }
 }
 
