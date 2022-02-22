17ad4a40e87ca9d5a78ffd669bc2cb52f5b5e95e
ahza64 <justinmichaelpettit@gmail.com>
2022-02-22 11:26:42 -0800

    isAnagram

--- LeetCode_JS_Classic/exercises/LC242_isAnagram/index.js
+++ LeetCode_JS_Classic/exercises/LC242_isAnagram/index.js
@@ -1,3 +1,25 @@
-function isAnagram(s, t) {}
+function isAnagram(s, t) {
+  if (s.length !== t.length) {
+    return false
+  }
+
+  const sCharCounts = {}
+
+  for (let i = 0; i < s.length; i++) {
+    const sChar = s[i]
+    sCharCounts[sChar] = sCharCounts[sChar] + 1 || 1
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    const tChar = t[i]
+
+    if (!sCharCounts[tChar]) {
+      return false
+    } else {
+      sCharCounts[tChar]--
+    }
+  }
+  return true
+}
 
 module.exports = isAnagram;
