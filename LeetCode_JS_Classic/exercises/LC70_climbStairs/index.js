b8b1bb02bc0ea0cca391f7c763603d6add05b0a7
ahza64 <justinmichaelpettit@gmail.com>
2022-02-23 11:30:15 -0800

    leet climbStairs with better space complexity option

--- LeetCode_JS_Classic/exercises/LC70_climbStairs/index.js
+++ LeetCode_JS_Classic/exercises/LC70_climbStairs/index.js
@@ -1,3 +1,25 @@
-const climbStairs = n => {};
+const climbStairs = n => {
+  // if (n <= 3) return n
+  //
+  // let ways = [0, 1, 2, 3]
+  // for (let i = 4; i <= n; i++) {
+  //   ways.push(ways[i - 1] + ways[i - 2])
+  // }
+  // return ways.pop()
+  // better space complexity
+  if (n === 1) {
+    return n
+  }
+
+  let first = 1
+  let second = 2
+
+  for (let i = 3; i <= n; i++) {
+    const third = first +  second
+    first = second
+    second = third
+  }
+  return second
+};
 
 module.exports = climbStairs;
