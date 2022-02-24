3f94fa280fe518215d4a54dc4b16951c5176043e
ahza64 <justinmichaelpettit@gmail.com>
2022-02-24 12:54:09 -0800

    leet longest increasing subsequence

--- LeetCode_JS_Classic/exercises/LC300_lengthOfLIS/index.js
+++ LeetCode_JS_Classic/exercises/LC300_lengthOfLIS/index.js
@@ -1,3 +1,18 @@
-function lengthOfLIS(nums) {}
+function lengthOfLIS(nums) {
+  if (nums.length === 0) return 0
+
+  let dpSubsecuence = new Array(nums.length).fill(1)
+  let maxSoFar = 1
+
+  for (let j = 0; j < nums.length; j++) {
+    for (let i = 0; i < j; i++) {
+      if (nums[j] > nums[i]) {
+        dpSubsecuence[j] = Math.max(dpSubsecuence[i] + 1, dpSubsecuence[j])
+      }
+    }
+    maxSoFar = Math.max(maxSoFar, dpSubsecuence[j])
+  }
+  return maxSoFar
+}
 
 module.exports = lengthOfLIS;
