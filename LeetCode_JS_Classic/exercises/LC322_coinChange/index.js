9c3b8b9df4913b033d278cd9669b03276cfd122e
ahza64 <justinmichaelpettit@gmail.com>
2022-02-27 18:40:03 -0800

    leet coinChange

--- LeetCode_JS_Classic/exercises/LC322_coinChange/index.js
+++ LeetCode_JS_Classic/exercises/LC322_coinChange/index.js
@@ -1,3 +1,17 @@
-function coinChange(coins, amount) {}
+function coinChange(coins, amount) {
+  let dpMinCoins = new Array(amount + 1).fill(Infinity)
+  dpMinCoins[0] = 0
+
+  for (let i = 0; i < dpMinCoins.length; i++) {
+    for (let j = 0; j < coins.length; j++) {
+      const coinVal = coins[j]
+      if (coinVal <= i) {
+        dpMinCoins[i] = Math.min(dpMinCoins[i - coinVal] + 1, dpMinCoins[i] )
+      }
+    }
+  }
+  const answer = dpMinCoins[dpMinCoins.length - 1]
+  return answer === Infinity ? -1 : answer
+}
 
 module.exports = coinChange;
