5759a09fb75a18713291d4215ba41b0c794617ac
ahza64 <justinmichaelpettit@gmail.com>
2022-04-04 23:54:18 -0700

    OOP challenge 2

--- complete-javascript-course/14-OOP/starter/script.js
+++ complete-javascript-course/14-OOP/starter/script.js
@@ -1,17 +1,19 @@
 'use strict';
 
+
+/* Challenge 1 */
 const Car = function (make, speed) {
   this.make = make
   this.speed = speed
 }
 
 Car.prototype.accelerate = function () {
-  this.speed = this.speed + 10
+  this.speed += 10
   console.log("accelerate", this.speed);
 }
 
 Car.prototype.brake = function () {
-  this.speed = this.speed - 5
+  this.speed -= 5
   console.log("brake", this.speed);
 }
 
@@ -20,3 +22,30 @@ const car2 = new Car("chevy", 50)
 
 car1.brake()
 car2.brake()
+
+
+/* Challenge 2 */
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make
+    this.speed = speed
+  }
+
+  accelerate() {
+    this.speed += 10
+    console.log("acc", this.speed);
+  }
+
+  brake() {
+    this.speed -= 5
+    console.log("bk", this.speed);
+  }
+
+  get speedUS() {
+    return this.speed * 1.6
+  }
+}
+
+const car3 = new CarCl("buick", 60)
+car3.brake()
