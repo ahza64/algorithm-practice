7a9eefc1ab000cf34946606e261ba181b7902e66
ahza64 <justinmichaelpettit@gmail.com>
2022-04-05 10:32:13 -0700

    OOP challenge 3, prototype/__proto__ inheritance chain implimentation with poly morphizem using function expression (declaration works the same), with constructor function and the 'new' keyword. no ES6 class' yet

--- complete-javascript-course/14-OOP/starter/script.js
+++ complete-javascript-course/14-OOP/starter/script.js
@@ -20,8 +20,8 @@ Car.prototype.brake = function () {
 const car1 = new Car("ford", 100)
 const car2 = new Car("chevy", 50)
 
-car1.brake()
-car2.brake()
+// car1.brake()
+// car2.brake()
 
 
 /* Challenge 2 */
@@ -45,7 +45,40 @@ class CarCl {
   get speedUS() {
     return this.speed * 1.6
   }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6
+  }
 }
 
 const car3 = new CarCl("buick", 60)
-car3.brake()
+// car3.brake()
+// car3.speedUS = 100
+// console.log(car3);
+
+
+/* Challenge 3 */
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed)
+  this.charge = charge
+}
+
+EV.prototype = Object.create(Car.prototype)
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo
+  console.log(`${this.charge} battery charge level`);
+}
+
+Ev.prototype.accelerate = function () {
+  this.speed += 20
+  this.charge--
+  console.log(`${this.make} is going ${this.speed} with charge ${this.charge}`);
+}
+
+const tesla = new EV('Tesla', 120, 23)
+tesla.accelerate()
+tesla.chargeBattery(90)
+tesla.accelerate()
+tesla.brake()
+console.log(tesla);
