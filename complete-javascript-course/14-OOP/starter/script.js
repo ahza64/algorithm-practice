60f218885b8f9b32bcb4ff135c455d4c6cacea49
ahza64 <justinmichaelpettit@gmail.com>
2022-04-05 12:21:05 -0700

    OOP challenge 4, ES6 class extends constructor super return this chaining private class fields pretty much everything advanced OOP

--- complete-javascript-course/14-OOP/starter/script.js
+++ complete-javascript-course/14-OOP/starter/script.js
@@ -2,6 +2,7 @@
 
 
 /* Challenge 1 */
+
 const Car = function (make, speed) {
   this.make = make
   this.speed = speed
@@ -40,6 +41,7 @@ class CarCl {
   brake() {
     this.speed -= 5
     console.log("bk", this.speed);
+    return this
   }
 
   get speedUS() {
@@ -58,11 +60,13 @@ const car3 = new CarCl("buick", 60)
 
 
 /* Challenge 3 */
+
 const EV = function (make, speed, charge) {
   Car.call(this, make, speed)
   this.charge = charge
 }
 
+// set Car prototype properties to EV, otherwise EV will just share the same prototype as Car and break the protypial chain
 EV.prototype = Object.create(Car.prototype)
 
 EV.prototype.chargeBattery = function (chargeTo) {
@@ -70,15 +74,45 @@ EV.prototype.chargeBattery = function (chargeTo) {
   console.log(`${this.charge} battery charge level`);
 }
 
-Ev.prototype.accelerate = function () {
+// use polymorphism to set an updated "accelerate()" lower on the protypial chain, effectively invoking the call before javascript reads any higher up the prototype chain to find the older Car version of accelerate() on line 10
+EV.prototype.accelerate = function () {
   this.speed += 20
   this.charge--
   console.log(`${this.make} is going ${this.speed} with charge ${this.charge}`);
 }
 
 const tesla = new EV('Tesla', 120, 23)
-tesla.accelerate()
-tesla.chargeBattery(90)
-tesla.accelerate()
-tesla.brake()
-console.log(tesla);
+// tesla.accelerate()
+// tesla.chargeBattery(90)
+// tesla.accelerate()
+// tesla.brake()
+// console.log(tesla);
+
+
+/* Challenge 4 */
+
+class EVCl extends CarCl {
+  #charge; // private class field
+
+  constructor(make, speed, charge) {
+    super(make, speed) // add CarCl prototype properties to the EVCl prototype
+    this.#charge = charge
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo
+    console.log(`battery charged to ${this.#charge}`);
+    return this //returning this makes this method chainable
+  }
+
+  accelerate() {
+    this.speed += 20
+    this.#charge--
+    console.log(`${this.make} is going ${this.speed} with charge ${this.#charge}`);
+    return this
+  }
+}
+
+const rivian = new EVCl("Rivian", 90, 80)
+rivian.accelerate().accelerate().brake().chargeBattery(90).brake().accelerate()
+console.log(rivian);
